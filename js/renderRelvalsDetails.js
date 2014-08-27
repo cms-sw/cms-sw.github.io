95c9b12cad170015707afd2b3d168a5bd517d938
cmsbuild@cern.ch <cmsbuild@cern.ch>
2014-08-27 10:57:48 +0200

    Test version. 20140827-1057

--- js/renderRelvalsDetails.js
+++ js/renderRelvalsDetails.js
@@ -3,34 +3,41 @@
 // ------------------------------------------------------------------------------
 
 /**
- * Adds a row with the summary of the results
+ * generates the function for a given arch and a given Ib to take into account the different ids for each case
  */
-addSummaryRow = function( table , statistics ){
+genAddSummaryRow = function( genArch , genIB ){
 
-  var row = $( '<tr>' )
-  row.append( $( '<td>' ) )
+  /**
+   * Adds a row with the summary of the results
+  */
+  return function( table , statistics ){
 
-  var showSummaryLink = getLinkWithGlyph( '#' + ARCH + ';' + IB , 'Show summary' , 'glyphicon-chevron-right' , 'toggleSummaryLink' )
+    var row = $( '<tr>' )
+    row.append( $( '<td>' ) )
 
-  var labelsTable = $( '<table>' ).attr( 'id' , 'summarylabelsTable'  ).attr( 'align' ,'right' ) 
-  labelsTable.append( $( '<tr>' ).append( $( '<td>' ).text( 'Passed:' ) ) )
-  labelsTable.append( $( '<tr>' ).append( $( '<td>' ).text( 'Failed:' ) ) )
-  labelsTable.append( $( '<tr>' ).append( $( '<td>' ).text( 'Not run:' ) ) )
-  labelsTable.append( $( '<tr>' ).append( $( '<td>' ).text( 'Total:' ) ) )
+    var showSummaryLink = getLinkWithGlyph( '#' + genArch + ';' + genIB , 'Show summary' , 'glyphicon-chevron-right' , 'toggleSummaryLink'+ '-' + genArch + '-' + genIB )
 
-  var linkTable = $( '<table>' ).attr( 'align' ,'right' )
-  var linkTableRow = $( '<tr>' )
-  linkTableRow.append( $( '<td>' ).append( showSummaryLink ).attr( 'style' ,'text-align: right;' ) )
-  linkTableRow.append( $( '<td>' ).append( labelsTable ).attr( 'align' ,'right' ) )
-  linkTable.append( linkTableRow ) 
-  labelsTable.hide() 
+    showSummaryLink.click( genToggleSummaryTables( genArch , genIB ) )
 
-  row.append( $( '<td>' ).append( linkTable ) )
+    var labelsTable = $( '<table>' ).attr( 'id' , 'summarylabelsTable'  ).attr( 'align' ,'right' ) 
+    labelsTable.append( $( '<tr>' ).append( $( '<td>' ).text( 'Passed:' ) ) )
+    labelsTable.append( $( '<tr>' ).append( $( '<td>' ).text( 'Failed:' ) ) )
+    labelsTable.append( $( '<tr>' ).append( $( '<td>' ).text( 'Not run:' ) ) )
+    labelsTable.append( $( '<tr>' ).append( $( '<td>' ).text( 'Total:' ) ) )
 
-  for( var i = 0; i < 5; i++ ){
+    var linkTable = $( '<table>' ).attr( 'align' ,'right' )
+    var linkTableRow = $( '<tr>' )
+    linkTableRow.append( $( '<td>' ).append( showSummaryLink ).attr( 'style' ,'text-align: right;' ) )
+    linkTableRow.append( $( '<td>' ).append( labelsTable ).attr( 'align' ,'right' ) )
+    linkTable.append( linkTableRow ) 
+    labelsTable.hide() 
+  
+    row.append( $( '<td>' ).append( linkTable ) )
 
-    var summaryTable = $( '<table>' ).attr( 'id' , 'summaryStep' +( i+1 ) ).attr( 'align' ,'left' )
-    for ( key in statistics[ i ] ){
+    for( var i = 0; i < 5; i++ ){
+
+      var summaryTable = $( '<table>' ).attr( 'id' , 'summaryStep' +( i+1 ) +'-' + genArch + '-' + genIB ).attr( 'align' ,'left' )
+      for ( key in statistics[ i ] ){
 
         var summaryRow = $( '<tr>' )
         var statTitle = key.charAt( 0 ) + key.slice( 1 ).toLowerCase() + ': ' 
@@ -39,32 +46,45 @@ addSummaryRow = function( table , statistics ){
         summaryRow.append( valueCell )
         summaryTable.append( summaryRow )
 
+      }
+
+      row.append( $( '<td>' ).append( summaryTable ) )
+
+      summaryTable.hide()
     }
-
-    row.append( $( '<td>' ).append( summaryTable ) )
-
-    summaryTable.hide()
-  }
   
-  table.prepend( row )
+    table.prepend( row )
+
+  }
 
 }
 
 /**
- * Adds to the workflos table a row which has a link which toggles the workflows after the 20th
+ * Generates the function with the given arch and given IB to take them into account in the id and url
  */
-addShowAllRow = function( table , startRow ){
+genAddShowAllRowLink = function( genArch, genIB ){
 
-  var row = $( '<tr>' )
-  var linkCell = $( '<td>' ).attr( 'colspan' , 7 )
-  var showAllLink = getLinkWithGlyph( '#' + ARCH + ';' + IB  , 'Show All' , 'glyphicon-chevron-down' , 'showAllLink' )
+  /**
+   * Adds to the workflos table a row which has a link which toggles the workflows after the 20th
+   */
+  addShowAllRowLink = function( table , startRow , endRow ){
 
-  linkCell.append( showAllLink )
-  row.append( linkCell )
-  table.append( row )
+    var row = $( '<tr>' )
+    var linkCell = $( '<td>' ).attr( 'colspan' , 7 )
+    var showAllLink = getLinkWithGlyph( '#' + genArch + ';' + genIB  , 'Show All' , 'glyphicon-chevron-down' , 'showAllLink'+ '-' + genArch + '-' + genIB )
+
+    showAllLink.click( genToggleHiddenRows( genArch, genIB , startRow , endRow ) )
+
+
+    linkCell.append( showAllLink )
+    row.append( linkCell )
+    table.append( row )
+
+  }
+
+  return addShowAllRowLink
 
 }
-
 /**
  * returns the link address for a given Ib and an arch
  */
@@ -77,12 +97,22 @@ getlinkAddress = function( arch , ib , step , workflowName , workflowID ){
     filename = 'step' + ( parseInt( step ) + 1 ) + '_' + workflowName + '.log'
   }
    
-  console.log( step ) 
-  console.log( workflowName ) 
- 
   var address = 'http://cmssdt.cern.ch/SDT/cgi-bin/buildlogs/' + arch + '/' + ib + '/pyRelValMatrixLogs/run/' + workflowID + '_' + workflowName + '/' + filename
 
   return address;
+
+}
+
+/**
+ * returns a link to the result log of the relval with the label given as a parameter
+ */
+getLinkLabelToResultToResLabel = function( arch , ib , stepNumber , workflowName , workflowId , resLabel ){
+ 
+  var linkAddress = getlinkAddress( arch , ib , stepNumber , workflowName , workflowId )
+  var link = $( "<a>" ).attr( "href" , linkAddress )
+  link.append( resLabel )
+
+  return link
 
 }
 
@@ -92,7 +122,7 @@ getlinkAddress = function( arch , ib , step , workflowName , workflowID ){
  * The statistics is a array of dictionaries, each position has a dictionary with the entries
  * "passed" , "failed" and "notrun" with the numbers for the step
  */
-addWorkflowRow = function( workflowResult , table , counter , statistics , arch , ib ) {
+addWorkflowRow = function( workflowResult , table , counter , statistics , arch , ib , totalRows ) {
 
   var row = $( '<tr>' ).attr( 'id' , 'row' + counter + '-' + arch )
 
@@ -103,8 +133,6 @@ addWorkflowRow = function( workflowResult , table , counter , statistics , arch 
 
   // this is to fill all the rows with cells
   var numCells = 0;
-
-  console.log( linkAddress )
 
   var nothingRun = true;
   for ( var stepNumber in workflowResult.steps ){
@@ -118,19 +146,29 @@ addWorkflowRow = function( workflowResult , table , counter , statistics , arch 
       nothingRun = false;
       resLabel.attr( 'class' , 'label label-success')
 
+      var link = getLinkLabelToResultToResLabel( arch , ib , stepNumber , workflowResult.name , workflowResult.id , resLabel )
+      row.append( $( '<td>' ).append( link ) )
+
+
     }else if( text == 'FAILED' ){
 
       nothingRun = false;
       resLabel.attr( 'class' , 'label label-danger')
       row.attr( 'class' , 'danger' )
 
+      var link = getLinkLabelToResultToResLabel( arch , ib , stepNumber , workflowResult.name , workflowResult.id , resLabel )
+      row.append( $( '<td>' ).append( link ) )
+
+
     }else if( text == 'NOTRUN' ){
 
       resLabel.attr( 'class' , 'label label-default')
+      row.append( $( '<td>' ).append( resLabel ) )
 
     }else {
 
       resLabel.attr( 'class' , 'label label-default')
+      row.append( $( '<td>' ).append( resLabel ) )
 
     }
 
@@ -141,12 +179,7 @@ addWorkflowRow = function( workflowResult , table , counter , statistics , arch 
       statistics[ stepNumber ][ "TOTAL" ]++;
     }
 
-    var linkAddress = getlinkAddress( arch , ib , stepNumber , workflowResult.name , workflowResult.id )
-    console.log( linkAddress )
-    var link = $( "<a>" ).attr( "href" , linkAddress )
-    link.append( resLabel )
 
-    row.append( $( '<td>' ).append( link ) )
     numCells++;
   }
 
@@ -167,7 +200,9 @@ addWorkflowRow = function( workflowResult , table , counter , statistics , arch 
   table.append( row )
 
    if ( counter == 20 ){
-    addShowAllRow( table , 20 )
+
+    addShowAllRowLink = genAddShowAllRowLink( arch , ib )
+    addShowAllRowLink( table , 20 , totalRows )
   }
 
   return false;
@@ -180,7 +215,7 @@ addWorkflowRow = function( workflowResult , table , counter , statistics , arch 
 getTable = function( results , arch , ib ){
 
   var table = $( '<table>' ).attr( 'class' , 'table table-striped table-condensed' )
-  table.attr( 'id' , 'resultsTable;' + arch + ';' + ib ) 
+  table.attr( 'id' , 'resultsTable-' + arch + '-' + ib ) 
 
   addHeaderToTable( table )
 
@@ -198,12 +233,13 @@ getTable = function( results , arch , ib ){
   var counter = 1;
   for ( var key in results ){
     // nothingRun is to know if no step was run in the workflow
-    nothingRun = addWorkflowRow( results[ key ] , table , counter , resultsSummary , arch , ib )
+    nothingRun = addWorkflowRow( results[ key ] , table , counter , resultsSummary , arch , ib , results.length )
     if ( !nothingRun ){
       counter++;
     }
   }
 
+  addSummaryRow = genAddSummaryRow( arch , ib ) 
   addSummaryRow( table , resultsSummary )
  
   return table
@@ -236,6 +272,133 @@ addHeaderToTable = function ( table ) {
 }
 
 //-------------------------------------------------------------------------------
+// Tabs
+// ------------------------------------------------------------------------------
+
+/**
+ * generates a function to show the tab with the id given as a parameter
+ */
+genShowTab = function( tabLink ){
+
+  /**
+   * shows the tab with the id given as a parameter
+   **/
+  showTab = function( e ){
+    e.preventDefault( )
+    tabLink.tab('show')
+  }
+
+  return showTab
+
+}
+
+
+/**
+ * Creates the navtabs with based on the archsList the ibName is for creating the link
+ */
+getNavTabs = function( archsList , ibName ){
+
+  var tabList = $( '<ul>' ).attr( 'class', 'nav nav-tabs').attr( 'role' , 'tablist')
+
+  var oneSelected = false;
+  for( var i = 0; i < archsList.length ; i ++){
+
+    var item = $( '<li>' )
+    var archName = archsList[ i ]
+
+    // if the it is the requested arch it is active
+    if( archName == ORIGINAL_ARCH ){
+      item.attr( 'class' , 'active' )
+      oneSelected = true
+      CURRENT_ARCH = archName
+    }
+
+    // if none of them is requested the last one is the onq which becomes active
+    if ( i == archsList.length -1 && !oneSelected ){
+      item.attr( 'class' , 'active' )
+      CURRENT_ARCH = archName
+    }
+
+
+    var tabLink = $( '<a>' ).attr( 'href' , '#' + archName + '-tab' )
+    
+    //var itemID = archName + '-' + ibName + '-tabLiItem'
+   // item.attr( 'id' , itemID )
+   // var tabID = archName + '-' + ibName + '-tab'
+    tabLink.click( genShowTab( tabLink ) )
+
+    tabLink.text( archName )
+    
+    item.append( tabLink )
+    tabList.append( item )
+  }
+
+  
+
+
+  return tabList
+}
+
+/**
+ * It generates a function that 
+ * Reads the results of one file, gets the table, and appends it to the tab pane
+ */
+generateAddResultsTableToPane = function( tabPaneID , arch , ibName ){
+
+  return function( results ){
+
+    console.log ( 'modifying' )
+    console.log ( tabPaneID )
+    var table = getTable( results , arch , ibName )
+
+    $( '#' + tabPaneID ).append( table )
+  }
+
+}
+
+
+
+/**
+ * Creates the tab panes based on the archsList and the IBName
+ */
+getTabPanes = function( archsList , ibName ){
+
+  var tabContent = $( '<div>' ).attr( 'class' , 'tab-content' )
+
+
+  for( var i = 0; i < archsList.length ; i++){
+
+    var tabPaneClass = 'tab-pane'
+    var arch = archsList[ i ]
+
+    if ( arch == CURRENT_ARCH ){
+      tabPaneClass += ' active'
+    }
+   
+    var tabPaneID = arch + '-tab' 
+    var tabPane = $( '<div>' ).attr( 'class' , tabPaneClass ).attr( 'id' , tabPaneID ) 
+    
+    var ibDate = ibName.substring( ibName.lastIndexOf( "_" ) + 1 , ibName.length ) 
+    var releaseQueue = ibName.substring( 0 , ibName.lastIndexOf( "_" ) )
+    var jsonFilePath = 'data/relvals/' + arch + '/' + ibDate + '/' + releaseQueue +'.json';
+
+    var addResultsTableToPane = generateAddResultsTableToPane( tabPaneID , arch , ibName )
+
+    console.log( 'Reading: ' )
+    console.log( jsonFilePath )
+    $.getJSON( jsonFilePath , addResultsTableToPane )
+
+    console.log( 'Finished Reading: ' )
+
+    tabContent.append( tabPane )
+  }
+
+  return tabContent
+
+
+}
+
+//-------------------------------------------------------------------------------
 // Header
 // ------------------------------------------------------------------------------
 
@@ -246,6 +409,7 @@ getHeader = function( arch, ibName ){
 
   var header = $( '<div>' )
   var title = $( '<h1>' ).text( 'Integration Build ' + ibName )
+
 
   header.append( title ).append( $( '<br>' ) )
 
@@ -270,3 +434,119 @@ getLinkWithGlyph = function( linkAddress, text, glyph , id ){
 
 }
 
+/**
+ * Generates a header that informs the user that there were not found results for the given IB
+ */
+getNotFoundIBHeader = function( ibName ){
+
+ var div = $( '<div>' ).attr( 'class' , 'alert alert-danger' ).attr( 'role' , 'alert' ).attr( 'align' , 'center' )
+ div.text( 'No results were found for the IB ' )
+ div.append( $( '<strong>' ).text( ibName) )
+
+ 
+ return div
+
+
+}
+
+/**
+ * Generates a header that alerts that there were not found results for the given arch, anyway, 
+ * the results for the other archs are shown
+ */
+getNotFoundArchHeader = function( arch ){
+
+ var div = $( '<div>' ).attr( 'class' , 'alert alert-warning' ).attr( 'role' , 'alert' ).attr( 'align' , 'center' )
+ div.text( 'No results were found for the architecture ' )
+ div.append( $( '<strong>' ).text( arch ) )
+ 
+
+ return div
+
+
+}
+
+//-----------------------------------------------------------------------------
+// Helpers
+// ----------------------------------------------------------------------------
+
+/**
+ * determines if the list containes the string given as a parameter
+ * returns True if the list contains the string, False otherwise
+ */
+listContainsString = function( aString , list ){
+
+  for( var i = 0; i < list.length; i ++){
+
+    if ( list[ i ] == aString ){
+      return true
+    }
+  }
+
+  return false
+
+}
+
+//-----------------------------------------------------------------------------
+// Togglers
+// ----------------------------------------------------------------------------
+
+/**
+ * generates a toggler function for the summary table
+ */
+genToggleSummaryTables = function( genArch , genIB ){
+
+  toggleSummaryTables = function(){
+
+    for( var i = 0; i < 5; i++ ){
+
+      $( '#summaryStep' + (i+1) + '-' + genArch + '-' + genIB ).toggle()
+
+    }
+
+    $( '#summarylabelsTable-' + genArch + '-' + genIB ).toggle()
+
+    var toggleLinkText = $( '#span-toggleSummaryLink-' + genArch + '-' + genIB )
+    var toggleLinkTextGlyph = $( '#glyph-toggleSummaryLink-' + genArch + '-' + genIB )
+
+    if ( toggleLinkText.text() == 'Show summary' ){
+
+      toggleLinkText.text( 'Hide summary' )
+      toggleLinkTextGlyph.attr( 'class' , 'glyphicon glyphicon-chevron-up' )
+
+     }else {
+      toggleLinkText.text( 'Show summary' )
+      toggleLinkTextGlyph.attr( 'class' , 'glyphicon glyphicon-chevron-right' )
+     }
+  }
+
+  return toggleSummaryTables
+
+}
+
+/**
+ * generates a toggler function for the hidden rows
+ */
+genToggleHiddenRows = function( genArch , genIB , minRow , maxRow ){
+
+  toggleHiddenRows = function( ){
+
+    for( var i = minRow + 1; i < maxRow; i++ ){
+      $( '#row' + i + '-' + genArch ).toggle()
+   
+    }
+
+    var showAllLinkText = $( '#span-showAllLink-' + genArch + '-' + genIB )
+    var toggleLinkTextGlyph = $( '#glyph-showAllLink-' + genArch + '-' + genIB )
+
+    if ( showAllLinkText.text() == 'Show All' ){
+      showAllLinkText.text( 'Hide ' )
+      toggleLinkTextGlyph.attr( 'class' , 'glyphicon glyphicon-chevron-up' )
+
+    }else {
+      showAllLinkText.text( 'Show All' )
+      toggleLinkTextGlyph.attr( 'class' , 'glyphicon glyphicon-chevron-down' )
+    }
+  }
+
+  return toggleHiddenRows
+}
